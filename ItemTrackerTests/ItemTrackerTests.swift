a42091c0f66b6478506368c34a6da0a1410c10df
Srikanth Sombhatla <sriks.memberships@gmail.com>
2025-10-17 11:47:53 +1100

    Initial Commit

---
+++ ItemTrackerTests/ItemTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ItemTrackerTests.swift
+//  ItemTrackerTests
+//
+//  Created by Srikanth on 17/10/2025.
+//
+
+import Testing
+@testable import ItemTracker
+
+struct ItemTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
